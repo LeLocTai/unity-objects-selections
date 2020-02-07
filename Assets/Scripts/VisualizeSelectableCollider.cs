89cd6a62a500da5698b07d6ed8ca12d166142df5
Tai Le <contact@leloctai.com>
2020-02-07 15:15:54 +0700

    custom visualize color

--- Assets/Scripts/VisualizeSelectableCollider.cs
+++ Assets/Scripts/VisualizeSelectableCollider.cs
@@ -7,13 +7,15 @@ namespace LeTai.Selections
 [RequireComponent(typeof(Renderer))]
 public class VisualizeSelectableCollider : MonoBehaviour
 {
+    public Color selectedColor;
+    public Color deselectedColor;
     void Start()
     {
         var selectableCollider = GetComponent<SelectableCollider>();
         var material           = GetComponent<Renderer>().material;
 
-        selectableCollider.selected   += () => material.color = Color.green;
-        selectableCollider.deselected += () => material.color = Color.white;
+        selectableCollider.selected   += () => material.color = selectedColor;
+        selectableCollider.deselected += () => material.color = deselectedColor;
     }
 }
 }
