6e344316b27b7ee7060f4ea6b94df3a09a59ce54
Tai Le <contact@leloctai.com>
2020-02-08 11:44:20 +0700

    support instanced material

--- Assets/Scripts/VisualizeSelectableCollider.cs
+++ Assets/Scripts/VisualizeSelectableCollider.cs
@@ -9,13 +9,24 @@ public class VisualizeSelectableCollider : MonoBehaviour
 {
     public Color selectedColor;
     public Color deselectedColor;
+
+    new Renderer          renderer;
+    MaterialPropertyBlock selectedBlock;
+    MaterialPropertyBlock deselectedBlock;
+
     void Start()
     {
         var selectableCollider = GetComponent<SelectableCollider>();
-        var material           = GetComponent<Renderer>().material;
+        renderer = GetComponent<Renderer>();
 
-        selectableCollider.selected   += () => material.color = selectedColor;
-        selectableCollider.deselected += () => material.color = deselectedColor;
+        selectedBlock   = new MaterialPropertyBlock();
+        deselectedBlock = new MaterialPropertyBlock();
+
+        selectedBlock.SetColor("_Color", selectedColor);
+        deselectedBlock.SetColor("_Color", deselectedColor);
+
+        selectableCollider.selected   += () => renderer.SetPropertyBlock(selectedBlock);
+        selectableCollider.deselected += () => renderer.SetPropertyBlock(deselectedBlock);
     }
 }
 }
